2802bfa6e356b3349ce66dea8c51813e2c9dcbe9
jun <alap_u@naver.com>
2023-04-30 21:44:09 +0900

    BOJ14889.java

--- baekjoon/src/BOJ14889.java
+++ baekjoon/src/BOJ14889.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class BOJ14889 {
     private static int N;
     private static int MIN = Integer.MAX_VALUE;
     private static int[][] arr;
