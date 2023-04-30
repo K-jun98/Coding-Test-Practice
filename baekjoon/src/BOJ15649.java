8f312e6f62eaf5fa4e4040f53fb7d8cf3eb2eafb
jun <alap_u@naver.com>
2023-05-01 01:45:18 +0900

    BOJ15649.java

---
+++ baekjoon/src/BOJ15649.java
@@ -0,0 +1,37 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class BOJ15649 {
+    static int N;
+    static int M;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        List<Integer> list = new LinkedList<>();
+        matching(list);
+    }
+
+    private static void matching(List<Integer> list) {
+        if (list.size() == M) {
+            for (Integer k : list) {
+                System.out.printf(k+" ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+                if (list.contains(i)) {
+                    continue;
+                }
+                list.add(i);
+                matching(list);
+                list.remove(list.size() - 1);
+            }
+        }
+    }
+
