9c63614e6a829b0bb86fe416572be3f7fac778b6
jun <alap_u@naver.com>
2023-05-02 00:16:52 +0900

    BOJ2231.java

---
+++ baekjoon/src/BOJ2231.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.println(check(N));
+
+    }
+
+    private static int check(int N) {
+        for (int i = 0; i < N; i++) {
+            int temp = i;
+            int sum = i;
+            while (temp != 0) {
+                sum+=temp%10;
+                temp = temp / 10;
+            }
+            if (sum == N) {
+                return i;
+            }
+
+        }
+        return 0;
+    }
+}
