bd1244bfc625ce8fed64fbdaaf48de9dbc8fdd2f
jun <alap_u@naver.com>
2023-12-09 23:32:44 +0900

    BOJ14891.java

--- baekjoon/src/BOJ14891.java
+++ baekjoon/src/BOJ14891.java
@@ -1,7 +1,9 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class BOJ14891 {
     static int[][] arr = new int[4][8];
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
