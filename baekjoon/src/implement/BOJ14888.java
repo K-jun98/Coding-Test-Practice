8463a7574adaadb0488ee8d38d77b5ce1c68c063
jun <alap_u@naver.com>
2023-12-13 03:19:16 +0900

    BOJ14888.java

---
+++ baekjoon/src/implement/BOJ14888.java
@@ -0,0 +1,58 @@
+package implement;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BOJ14888 {
+
+    static int N;
+    static int[] E;
+    static int[] O = new int[4];
+
+    static int MAX = Integer.MIN_VALUE;
+    static int MIN = Integer.MAX_VALUE;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        sc.nextLine();
+        E = new int[N];
+        E = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        O = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        makeValue(0, E[0]);
+        System.out.println(MAX);
+        System.out.println(MIN);
+
+    }
+
+    public static void makeValue(int index, int value) {
+        if (index +1 == N) {
+            MAX = Math.max(MAX, value);
+            MIN = Math.min(MIN, value);
+            return;
+        }
+
+        for (int i = 0; i < 4; i++) {
+            if (O[i] != 0) {
+                O[i]--;
+                switch (i) {
+                    case 0:
+                        makeValue(index + 1, value + E[index + 1]);
+                        break;
+                    case 1:
+                        makeValue(index + 1, value - E[index + 1]);
+                        break;
+                    case 2:
+                        makeValue(index + 1, value * E[index + 1]);
+                        break;
+                    case 3:
+                        makeValue(index + 1, value / E[index + 1]);
+                }
+                O[i]++;
+            }
+
+        }
+
+    }
+
+}
