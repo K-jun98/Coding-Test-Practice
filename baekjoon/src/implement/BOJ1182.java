e6efb90ac1797898c45a129935a8c461df10b347
jun <alap_u@naver.com>
2023-12-31 22:23:19 +0900

    BOJ1182.java

---
+++ baekjoon/src/implement/BOJ1182.java
@@ -0,0 +1,48 @@
+package implement;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class BOJ1182 {
+
+    static int N, S;
+
+    static int[] array;
+
+    static int answer;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        array = new int[N];
+
+        S = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            array[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dfs(0, 0);
+        if (S == 0) answer -= 1;
+        System.out.println(answer);
+    }
+
+    static void dfs(int start, int sum) {
+        if (start == N) {
+            if (sum == S)
+                answer++;
+            return;
+        }
+        dfs(start + 1, sum);
+        dfs(start + 1, sum + array[start]);
+    }
+
+}
