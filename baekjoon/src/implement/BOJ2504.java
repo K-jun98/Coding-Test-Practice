dbe6e8c5cb74b50ea8a74f5ae4b11366c7af0942
jun <alap_u@naver.com>
2024-01-05 14:02:52 +0900

    BOJ2504.java

--- baekjoon/src/implement/BOJ2504.java
+++ baekjoon/src/implement/BOJ2504.java
@@ -1,59 +1,59 @@
 package implement;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Scanner;
 import java.util.Stack;
 
 public class BOJ2504 {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        System.out.println(calculateBracketValue(input));
-    }
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    private static int calculateBracketValue(String s) {
+    static int bracket(String s) {
         Stack<Character> stack = new Stack<>();
-        int value = 0;
-        int temp = 1;
-
-        for (int i = 0; i < s.length(); i++) {
-            switch (s.charAt(i)) {
-                case '(':
-                    stack.push('(');
-                    temp *= 2;
-                    break;
+        int[] p = new int[30];
+        for(char c : s.toCharArray()) {
+            switch (c) {
                 case '[':
-                    stack.push('[');
-                    temp *= 3;
+                    stack.push(c);
                     break;
-                case ')':
-                    if (stack.isEmpty() || stack.peek() != '(') {
-                        return 0;
-                    }
-                    if (s.charAt(i - 1) == '(') {
-                        value += temp;
-                    }
-                    stack.pop();
-                    temp /= 2;
+                case '(':
+                    stack.push(c);
                     break;
                 case ']':
-                    if (stack.isEmpty() || stack.peek() != '[') {
+                    if(stack.empty()||stack.peek()!='[') {
                         return 0;
                     }
-                    if (s.charAt(i - 1) == '[') {
-                        value += temp;
+                    stack.pop();
+                    p[stack.size()] += p[stack.size() + 1] == 0 ? 3 : p[stack.size() + 1] * 3;
+                    p[stack.size() + 1] = 0;
+                    break;
+                case ')':
+                    if(stack.empty()||stack.peek()!='(') {
+                        return 0;
                     }
                     stack.pop();
-                    temp /= 3;
+                    p[stack.size()] += p[stack.size() + 1] == 0 ? 2 : p[stack.size() + 1] * 2;
+                    p[stack.size() + 1] = 0;
+                    break;
+
+                default:
                     break;
             }
         }
 
-        if (!stack.isEmpty()) {
-            return 0;
-        }
+        return p[0];
+    }
 
-        return value;
+    public static void main(String[] args) throws Exception {
+        String s = br.readLine();
+        bw.write(String.valueOf(bracket(s)));
+
+        bw.close();
+        br.close();
     }
 
 }
