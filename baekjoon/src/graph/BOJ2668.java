2c4159787a40648820d56038e7e5c972e89c8ee1
jun <alap_u@naver.com>
2024-01-23 20:56:48 +0900

    BOJ2668.java

---
+++ baekjoon/src/graph/BOJ2668.java
@@ -0,0 +1,57 @@
+package graph;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class BOJ2668 {
+
+    static int N;
+
+    static ArrayList<Integer> list = new ArrayList<>();
+    static int[] arr;
+    static boolean[] visit;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N + 1];
+        visit = new boolean[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        for (int i = 1; i <= N; i++) {
+            visit[i] = true;
+            dfs(i, i);
+            visit[i] = false;
+        }
+
+        Collections.sort(list);
+        System.out.println(list.size());
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }
+
+    public static void dfs(int start, int target) {
+        int value = arr[start];
+
+        if (arr[start] == target) {
+            list.add(target);
+            return;
+        }
+
+        if (visit[value] == false) {
+            visit[value] = true;
+            dfs(value, target);
+            visit[value] = false;
+        }
+    }
+}
